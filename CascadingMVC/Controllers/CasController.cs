dc67adbbd8e7d1b0487a2f888f52b8b433f30eb1
MR-ADITYA <MR-ADITYA@DESKTOP-IONPEBJ>
2020-08-12 14:39:48 +0530

    Add project files.

---
+++ CascadingMVC/Controllers/CasController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CascadingMVC.Models;
+
+namespace CascadingMVC.Controllers
+{
+    public class CasController : Controller
+    {
+        // GET: Cas
+        public ActionResult Index()
+        {
+            SampleDBEntities sd = new SampleDBEntities();
+            ViewBag.countryList = new SelectList(GetCountryList(),"cid","cname");
+            return View();
+        }
+        public List<country> GetCountryList()
+        {
+            SampleDBEntities sd = new SampleDBEntities();
+            List<country> countries = sd.countries.ToList();
+            return countries;
+        }
+        public ActionResult GetStateList(int Cid)
+        {
+            SampleDBEntities sd = new SampleDBEntities();
+            List<state> selectList = sd.states.Where(x => x.cid== Cid).ToList();
+            ViewBag.Slist = new SelectList(selectList,"Sid","Sname");
+            return PartialView("DisplayStatess");
+        }
+        public ActionResult GetCityList(int Sid)
+        {
+            SampleDBEntities sd = new SampleDBEntities();
+            List<city> selectList = sd.cities.Where(x => x.sid == Sid).ToList();
+            ViewBag.Citylist = new SelectList(selectList, "Cityid", "Cityname");
+            return PartialView("DisplayCities");
+        }
+    }
+}
---
+++ CascadingMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CascadingMVC.MvcApplication" Language="C#" %>
